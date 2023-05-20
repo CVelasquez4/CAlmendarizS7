5217b802e471ceaf3d02200ced574bc2ae7c1d4a
CVelasquez4 <carloscesarlukas1987@hotmail.com>
2023-05-20 14:29:44 -0500

    Add project files.

---
+++ CAlmendarizS7/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CAlmendarizS7.Vistas;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CAlmendarizS7
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage ( new Vistas.Login());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CAlmendarizS7/DataBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite; //utiliza los metodos del gestor de base de datos
+
+namespace CAlmendarizS7
+{
+    public interface DataBase
+    {
+        SQLiteAsyncConnection GetConnection(); //defino el metodo de conexion
+    }
+}
